df90fd286017e6c14fc4dddfcc8d3d8b00c59a0c
Gabdulla <gabdulla2001@gmail.com>
2024-09-23 22:31:52 +0300

    add logging

--- src/file_readers.py
+++ src/file_readers.py
@@ -1,21 +1,34 @@
 import pandas as pd
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,  # Уровень логирования
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Формат вывода
+    filename='app.log',  # Логи будут сохраняться в файл 'app.log'
+    filemode='a'  # Режим записи (a - добавление к файлу)
+)
 
 
 def read_transactions_from_excel(file_path: str):
     """Читает данные из Excel и возвращает их в виде списка словарей."""
     try:
+        logging.info(f"Попытка чтения файла: {file_path}")
         df = pd.read_excel(file_path, engine='openpyxl')
         transactions = df.to_dict(orient='records')  # Преобразуем в список словарей
+        logging.info(f"Файл {file_path} успешно прочитан. Загружено {len(transactions)} транзакций.")
         return transactions
     except FileNotFoundError:
-        print(f"Файл {file_path} не найден.")
+        logging.error(f"Файл {file_path} не найден.")
         return []
     except Exception as e:
-        print(f"Ошибка чтения Excel-файла: {e}")
+        logging.error(f"Ошибка чтения Excel-файла: {e}")
         return []
 
 
 if __name__ == "__main__":
     transactions = read_transactions_from_excel("data/operations.xlsx")
-    print(transactions)
-
+    if transactions:
+        logging.info("Транзакции успешно загружены и готовы к обработке.")
+    else:
+        logging.warning("Не удалось загрузить транзакции.")
